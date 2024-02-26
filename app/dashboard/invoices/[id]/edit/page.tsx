7159b1101df4f4836cb96c0ebc82c96bfdf004d6
jxi <jiaxiang.xi@eisgroup.com>
2024-02-26 14:14:43 +0800

    chapter16: added meta data configs

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,27 @@ import Form from '@/app/ui/invoices/edit-form'
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data'
 import { notFound } from 'next/navigation'
+import { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+    params: { id: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+    { params, searchParams }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    // read route params
+    const id = params.id
+
+    // fetch data
+    const product = await fetchInvoiceById(id)
+
+    return {
+        title: `Edit Invoice: id: ${product.id}, ${product.amount} `
+    }
+}
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id
